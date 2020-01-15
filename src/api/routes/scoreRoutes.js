48fcdbcb1973577eeb26e1d2c1060ee88f7c3e7a
Tomas <t.mejia@ecole-ipssi.net>
2020-01-15 17:43:32 +0100

    Feat: added route for user, modules and session management & user view

--- src/api/routes/scoreRoutes.js
+++ src/api/routes/scoreRoutes.js
@@ -4,6 +4,6 @@ module.exports = (app) => {
   
     app.route('/score/:module?filtre=:session') // req.params.post_id
     .get(scoreControleur.get_all_modules)
-    .post(scoreControleur.set_a_score);
+    .post(scoresControleur.set_a_score);
   }
   
