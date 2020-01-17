77926527ac978bc30244e10845c41b9de1d79c40
Tomas <t.mejia@ecole-ipssi.net>
2020-01-17 12:06:38 +0100

    Remove route

--- src/api/routes/connectionRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/api/routes/userRoutes.js
-module.exports = (app) => {
-    const User = require('../controllers/userController');
-    const ARM = require('../middleware/accessRoleManagement');
-  
-    app.route('/connection/') 
-    .post(User.user_login);
-}
-  
