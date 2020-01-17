a1379f83e2f17d797c8c207ba942003b0918722a
Tomas <t.mejia@ecole-ipssi.net>
2020-01-17 09:53:29 +0100

    Feat: Added missing routes for the admin to see the scores and student to put a score

--- src/api/routes/userScoreRoutes.js
+++ src/api/routes/userScoreRoutes.js
@@ -1,13 +1,15 @@
 // src/api/routes/userRoutes.js
 module.exports = (app) => {
     const userController = require('../controllers/userController');
+    const scoreController = require('../controllers/scoreController');
+    const moduleController = require('../controllers/moduleController');
   
     app.route('/user/:id_user/score/:module') 
     .get(userController.get_all_modules)
     .get(userController.get_a_module)
 
     app.route('/user/:id_user/score/:module?filtre=:session') 
-    .post(userController.set_a_score);
+    .post(scoreController.set_a_score);
 
     app.route('/user/:id_user/psswd') 
     .put(userController.update_password) 
